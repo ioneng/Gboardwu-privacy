b73ad9ba0c1af28993684f49f6faa3ca242ff58d
ioneng <timfz@posteo.net>
2026-09-18 16:25:53 +1000

    feat: expose telemetry feature marker

--- extensions/extension/src/main/java/dev/jason/gboardpatches/extension/settings/GboardPatchesFeatureAvailability.java
+++ extensions/extension/src/main/java/dev/jason/gboardpatches/extension/settings/GboardPatchesFeatureAvailability.java
@@ -79,6 +79,8 @@ public final class GboardPatchesFeatureAvailability {
             "dev.jason.gboardpatches.feature.floating_web_search";
     public static final String FEATURE_SIMPLE_CALCULATOR =
             "dev.jason.gboardpatches.feature.simple_calculator";
+    public static final String FEATURE_TELEMETRY_BLOCKING =
+            "dev.jason.gboardpatches.feature.telemetry_blocking";
 
     private static final String TAG = "GboardPatches";
 
